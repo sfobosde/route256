17019b5a31f979750f984b39b72e8e10d0ce0e8f
Grigory Vasilev <grishavasilev62@gmail.com>
2024-02-08 21:16:13 +0300

    Решение

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -4,7 +4,24 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			using var input = new StreamReader(Console.OpenStandardInput());
+			using var output = new StreamWriter(Console.OpenStandardOutput());
+
+			const string expected = "4 3 3 2 2 2 1 1 1 1";
+
+			var count = Convert.ToInt32(input.ReadLine());
+			var response = "";
+
+			for (var i = 0; i < count; i++)
+			{
+				var ships = input.ReadLine().Split().ToList();
+
+				ships.Sort((a, b) => a.CompareTo(b) * -1);
+
+				response += (response.Length == 0 ? "" : "\n") + (String.Join(" ", ships.ToArray()) == expected ? "YES" : "NO");
+			}
+
+			output.Write(response);
 		}
 	}
 }
